19dfa1d46297f5932db94877f6b2d582d487e771
ponybai <pony.bai@gmail.com>
2023-02-02 15:59:06 +0800

    #install nest db package

--- server/apps/admin/src/app.module.ts
+++ server/apps/admin/src/app.module.ts
@@ -1,9 +1,10 @@
+import { DbModule } from '@libs/db';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
