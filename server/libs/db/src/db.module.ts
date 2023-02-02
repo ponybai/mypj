c02900102849dd83744c03f5a2d99c15c3ee64e3
ponybai <pony.bai@gmail.com>
2023-02-02 16:06:48 +0800

    #create user model

--- server/libs/db/src/db.module.ts
+++ server/libs/db/src/db.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DbService } from './db.service';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { User } from './models/user.model';
 
+const models = TypegooseModule.forFeature([User]);
+
+@Global()
 @Module({
-  imports:[ TypegooseModule.forRoot('mongodb://localhost/mypj')],
+  imports:[ TypegooseModule.forRoot('mongodb://localhost/mypj'),models],
   providers: [DbService],
-  exports: [DbService],
+  exports: [DbService, models],
 })
 export class DbModule {}
