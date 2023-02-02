34f9409062df84dfd412678b74b4098dccb658f4
ponybai <pony.bai@gmail.com>
2023-02-02 16:26:02 +0800

    #add module property

--- server/libs/db/src/models/user.model.ts
+++ server/libs/db/src/models/user.model.ts
@@ -1,8 +1,14 @@
 import { prop } from "@typegoose/typegoose";
+import { ApiProperty } from '@nestjs/swagger'
+
 
 export class User{
+
+    @ApiProperty({description:'user name', example:'user1'})
     @prop()
     username: string
+    
+    @ApiProperty({description:'Password', example:'pass1'})
     @prop()
     password: string
 }
