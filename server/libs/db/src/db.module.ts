4ce0d6f16025a7e085920633480e936d0a07676f
ponybai <pony.bai@gmail.com>
2023-02-02 16:04:13 +0800

    #connect db db.module.ts

--- server/libs/db/src/db.module.ts
+++ server/libs/db/src/db.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DbService } from './db.service';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
+  imports:[ TypegooseModule.forRoot('mongodb://localhost/mypj')],
   providers: [DbService],
   exports: [DbService],
 })
